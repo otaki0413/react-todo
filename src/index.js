fffc44e91b5ae2372b50c1cf251f995ee84e6615
otaki0413 <67969654+otaki0413@users.noreply.github.com>
2022-02-20 17:10:41 +0900

    commit section7-52 todoエリアに初期値を表示する際に、useStateを用いた。

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+// レンダリング先を指定
+ReactDOM.render(<App />, document.getElementById("root"));
