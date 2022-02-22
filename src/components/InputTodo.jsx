e521bd553991d734d85ab1c1f0e9ee4bb9a5ecd1
otaki0413 <67969654+otaki0413@users.noreply.github.com>
2022-02-22 23:48:30 +0900

    commit section7-58 コンポーネントファイルにCSSを移動するCSS-in-JSを実装した。

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+// CSS-in-JSを用いる
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px"
+};
+
 // 親の関数をpropsで渡す
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
